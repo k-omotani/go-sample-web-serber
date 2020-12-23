543f1e37f71a2b0b00239ea430a0529f5dd7e9b4
k-omotani <k.omotani.0225@gmail.com>
2020-12-24 00:01:37 +0900

    chiの実験

--- main.go
+++ main.go
@@ -2,23 +2,37 @@ package main
 
 import (
 	"fmt"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
 	"log"
 	"net/http"
 )
 
-
-func homePage(w http.ResponseWriter, r *http.Request){
-	fmt.Fprintf(w, "Welcome to the HomePage!")
-	fmt.Println("Endpoint Hit: homePage")
+func welcome(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to my app!")
 }
 
-func handleRequests() {
-	http.HandleFunc("/", homePage)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+func welcomeV1(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to v1!")
+}
+
+func helloWorld(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "hello world!")
 }
 
 func main() {
-	log.Printf("started applicaiton")
-	handleRequests()
-	log.Printf("shutdown applicaiton")
+	log.Printf("starting applicaiton...")
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Use(func(r http.Handler) http.Handler {
+		log.Printf("started application")
+		return r
+	})
+	r.Get("/", welcome)
+	r.Route("/v1", func(r chi.Router) {
+		r.Get("/", welcomeV1)
+		r.Get("/helloWorld", helloWorld)
+	})
+	http.ListenAndServe(":8000", r)
+	log.Printf("shutdown applicaiton...")
 }
