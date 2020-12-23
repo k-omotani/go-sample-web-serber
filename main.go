7eb8b71bcab0491dc927188ec055c8b4f724eef8
k-omotani <k.omotani.0225@gmail.com>
2020-12-23 23:25:58 +0900

    server 立ててみた

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+
+func homePage(w http.ResponseWriter, r *http.Request){
+	fmt.Fprintf(w, "Welcome to the HomePage!")
+	fmt.Println("Endpoint Hit: homePage")
+}
+
+func handleRequests() {
+	http.HandleFunc("/", homePage)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
 
 func main() {
-	fmt.Println("Hello world")
+	log.Printf("started applicaiton")
+	handleRequests()
+	log.Printf("shutdown applicaiton")
 }
